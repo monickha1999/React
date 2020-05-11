8635e028c183367d8935a64397f436351434828a
Monickha Rajan <monickhausha@gmail.com>
2020-05-11 13:22:41 +0530

    Updates

---
+++ precache-manifest.b72832bb41d452735b25d67c704ed234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717d89d44f4e5891eaba820737f319cf",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "621d1c8cb7b4e7bd1f0c",
+    "url": "/React/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "184380fd5624cfb85dc5",
+    "url": "/React/static/css/main.9d400f54.chunk.css"
+  },
+  {
+    "revision": "621d1c8cb7b4e7bd1f0c",
+    "url": "/React/static/js/2.3964cf68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.3964cf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184380fd5624cfb85dc5",
+    "url": "/React/static/js/main.c4e6d2a4.chunk.js"
+  },
+  {
+    "revision": "f892f8a717075f293151",
+    "url": "/React/static/js/runtime-main.511ad2b6.js"
+  }
+]);
